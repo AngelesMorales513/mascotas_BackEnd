b97135ba20399cfc208d6e3bad8eaa90284eac79
AngelesMorales513 <56412318+AngelesMorales513@users.noreply.github.com>
2021-08-13 01:30:57 -0500

    Cambios conexion
    Se realizaron cambios en la conexion a BD

--- build/src/config/keys.js
+++ build/src/config/keys.js
@@ -4,8 +4,8 @@ exports.default = {
     database: {
         host: 'mascotas-do-user-9674992-0.b.db.ondigitalocean.com',
         port: 25060,
-        user: 'doadmin',
-        password: 'kr12mh2ua81i8w21',
+        user: 'admin',
+        password: 'tfv646w2sr15bti7',
         database: 'mascotas'
     }
 };
--- src/config/keys.ts
+++ src/config/keys.ts
@@ -2,8 +2,8 @@ export default{
     database: {
         host: 'mascotas-do-user-9674992-0.b.db.ondigitalocean.com',
         port: 25060,
-        user: 'doadmin',
-        password: 'kr12mh2ua81i8w21',
+        user: 'admin',
+        password: 'tfv646w2sr15bti7',
         database: 'mascotas'
     }
 }
